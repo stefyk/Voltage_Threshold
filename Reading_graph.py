1a7dc904321911bd5b4e0357700f9635c8bba694
stefyk <43543157+stefyk@users.noreply.github.com>
2020-05-21 13:07:44 +0300

    Update Reading_graph.py

--- Reading_graph.py
+++ Reading_graph.py
@@ -1,5 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-csv = np.genfromtxt("Change in Frequency over time.csv", delimiter= ",")
-freq_data= csv [:,1]
-print (freq_data)
+data = np.genfromtxt("data.csv", delimiter=",")
+
+time = data[0][2:] # time values in time.time() format
+freq = data[1][2:] # also ignoring the first data entry
+
+starting_point = time[0]
+time = [point - starting_point for point in time] # making time start from 0
+
+# Plotting the data
+fig, ax = plt.subplots()
+ax.plot(time, freq)
+
+
+# Set labels
+ax.set(xlabel='time (s)', ylabel='Frequency diff (Hz)',
+       title='Frequency change')
+ax.grid()
+
+fig.savefig("Frequency.png")
+plt.show()
+
+print("Done")
